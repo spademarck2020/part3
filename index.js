90869268b3b2907d5dff0c999765980c117a6715
spademarck <spademarck2020@gmail.com>
2020-10-20 16:00:47 +0800

    part3a

---
+++ index.js
@@ -0,0 +1,98 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+morgan.token('content', (req,res)=>{return JSON.stringify(req.body)})
+
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
+
+const PORT = 9001
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendick",
+    number: "39-23-6423122",
+  }
+]
+
+app.get('/api/persons', (req,res) => {
+  console.log(persons)
+  res.json(persons)
+})
+
+app.get('/info', (req,res) => {
+  res.send(`<p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>
+  `)
+})
+
+app.get('/api/persons/:id', (req,res) => {
+  const id = Number(req.params.id)
+  
+  const person = persons.find(person => person.id === id)
+
+  if(person){
+    res.json(person)
+  }else{
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (req,res) => {
+  const id = Number(req.params.id)
+
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
+const generateId = () => {
+  return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
+}
+
+app.post('/api/persons', (req,res) => {
+  const body = req.body
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  }
+
+  const personExisting = persons.find(person => person.name === body.name)  
+
+  if(!body.name || !body.number){
+    return res.status(400).json({
+      error: 'Name or Number is missing...'
+    })
+  }else if(personExisting){
+    return res.status(409).json({
+      error: 'Name already exists'
+    })
+  }
+  
+    persons = persons.concat(person)
+    res.json(person).status(201)
+
+})
